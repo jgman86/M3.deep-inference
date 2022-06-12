cabdf740ecaf46a9e91db3a65680141dff1ec144
jgman86 <jan.goettmann@uni-mainz.de>
2022-06-12 23:52:29 +0200

    Added iterator function to M3_generator and model architecture

--- M3.py
+++ M3.py
@@ -4,57 +4,75 @@ nRet = 500
 noise = 0.1
 respOpt = [1, 4, 1, 4, 8]
 
+def generate_tmvn(batch_size=1, steps=10, size=1000, mu=np.zeros(3), cov=np.eye(3)):
+    # Truncation logic / Truncated MVN instead of MVN
+    i = 0
+    while i < steps:
+        i += 1
+        batch = np.random.multivariate_normal(mean=mu, cov=cov, size=(batch_size, size))
+        batch_tf = tf.convert_to_tensor(batch)  # dims: (batch_size, size, 3)
+        yield (batch_tf, tf.constant([1]))  # (data, label)
 
-def generate_m3(batchsize, stepsize, noise, respOpt, nRet):
-    # Sample Hyper Parameter
-    mu_c = np.random.uniform(1, 30, 1)
-    sig_c = np.random.uniform(0.125, 0.5) * mu_c
 
-    mu_a = np.random.uniform(0, 0.5, 1) * mu_c
-    sig_a = np.random.uniform(0.125, 0.5) * mu_a
 
-    mu_f = np.random.uniform(0, 1, 1)
-    log_mu_f = sp.special.logit(mu_f)
-    sig_f = np.random.uniform(0.1, 0.3, 1) * mu_f
+def generate_m3(batchsize, stepsize, steps, noise, respOpt, nRet):
+    i = 0
+    while i < steps:
+        i += 1
 
-    D = np.eye(4)
-    mu = np.array([mu_c[0], mu_a[0], log_mu_f[0], noise])
-    sig = np.diag([sig_c[0] ** 2, sig_a[0] ** 2, sig_f[0] ** 2, 0.001 ** 2])
-    lower = np.array([0, 0, -10e100, 0])
-    upper = np.array([10e100, 10e100, 10e100, 10e100])
+        # Sample Hyper Parameter
+        mu_c = np.random.uniform(1, 30, 1)
+        sig_c = np.random.uniform(0.125, 0.5) * mu_c
 
-    # Sample Parameters
-    theta = np.zeros((batchsize, stepsize, 4), dtype=float)
-    for i in range(0, batchsize):
-        theta[i, :, :] = rtmvn(stepsize, mu, sig, D, lower, upper, burn=100, thin=1)
+        mu_a = np.random.uniform(0, 0.5, 1) * mu_c
+        sig_a = np.random.uniform(0.125, 0.5) * mu_a
 
-    # Compute Activations
-    acts = np.zeros((batchsize, stepsize, 5), dtype=float)
+        mu_f = np.random.uniform(0, 1, 1)
+        log_mu_f = sp.special.logit(mu_f)
+        sig_f = np.random.uniform(0.1, 0.3, 1) * mu_f
 
-    for i in range(0, batchsize):
-        acts[i, :, 0] = noise + theta[i, :, 0] + theta[i, :, 1]
-        acts[i, :, 1] = noise + theta[i, :, 1]
-        acts[i, :, 2] = noise + sp.special.expit(theta[i, :, 2]) * (theta[i, :, 0] + theta[i, :, 1])
-        acts[i, :, 3] = noise + sp.special.expit(theta[i, :, 2]) * theta[i, :, 1]
-        acts[i, :, 4] = np.round_(theta[i, :, 3], 2)
+        D = np.eye(4)
+        mu = np.array([mu_c[0], mu_a[0], log_mu_f[0], noise])
+        sig = np.diag([sig_c[0] ** 2, sig_a[0] ** 2, sig_f[0] ** 2, 0.001 ** 2])
+        lower = np.array([0, 0, -10e100, 0])
+        upper = np.array([10e100, 10e100, 10e100, 10e100])
 
-    # acts[i, :, :] = np.column_stack((A_IIP, A_IOP, A_DIP, A_DIOP, A_NPL))
+        # Sample Parameters
+        theta = np.zeros((batchsize, stepsize, 4), dtype=float)
+        for i in range(0, batchsize):
+            theta[i, :, :] = rtmvn(stepsize, mu, sig, D, lower, upper, burn=100, thin=1)
 
-    # Compute Summed Acts and probabilites
-    def norm_acts(acts, respOpts):
-        sum_acts = sum(np.transpose(respOpts * np.transpose(acts)))
-        p = np.transpose(respOpts * np.transpose(acts)) / sum_acts
-        return p
+        # Compute Activations
+        acts = np.zeros((batchsize, stepsize, 5), dtype=float)
 
-    p = np.zeros((batchsize, stepsize, 5), dtype=float)
-    for i in range(0, batchsize):
-        p[i, :, :] = np.apply_along_axis(norm_acts, 1, acts[i, :, :], respOpt)
+        for i in range(0, batchsize):
+            acts[i, :, 0] = noise + theta[i, :, 0] + theta[i, :, 1]
+            acts[i, :, 1] = noise + theta[i, :, 1]
+            acts[i, :, 2] = noise + sp.special.expit(theta[i, :, 2]) * (theta[i, :, 0] + theta[i, :, 1])
+            acts[i, :, 3] = noise + sp.special.expit(theta[i, :, 2]) * theta[i, :, 1]
+            acts[i, :, 4] = np.round_(theta[i, :, 3], 2)
 
-    # Simulate Multinomial Data
-    data = np.zeros((batchsize, stepsize, 5), dtype=int)
+        # acts[i, :, :] = np.column_stack((A_IIP, A_IOP, A_DIP, A_DIOP, A_NPL))
 
-    for batch in range(0, batchsize):
-        for stepsize in range(0, stepsize):
-            data[batch, stepsize, :] = np.random.multinomial(nRet, p[batch, stepsize, :])
+        # Compute Summed Acts and probabilites
+        def norm_acts(acts, respOpts):
+            sum_acts = sum(np.transpose(respOpts * np.transpose(acts)))
+            p = np.transpose(respOpts * np.transpose(acts)) / sum_acts
+            return p
 
-    return data
+        p = np.zeros((batchsize, stepsize, 5), dtype=float)
+        for i in range(0, batchsize):
+            p[i, :, :] = np.apply_along_axis(norm_acts, 1, acts[i, :, :], respOpt)
+
+        # Simulate Multinomial Data
+        data = np.zeros((batchsize, stepsize, 5), dtype=int)
+
+        for batch in range(0, batchsize):
+            for stepsize in range(0, stepsize):
+                data[batch, stepsize, :] = np.random.multinomial(nRet, p[batch, stepsize, :])
+
+    # Merge Data and Theta
+
+    batch = #add sampled reference table theta and data
+    batch_tf = tf.convert_to_tensor(batch)  # dims: (batch_size, size, 3) # convert to tensor
+    yield (batch_tf, tf.constant([1]))  # (data, label)
--- train_model.py
+++ train_model.py
@@ -1,34 +1,114 @@
+# Import Modules
 import numpy as np
 import tensorflow as tf
-from keras import layers, models
+import datetime
+from tensorflow import keras
 
-def generate_tmvn(batch_size=1, steps=10, size=1000, mu=np.zeros(3), cov=np.eye(3)):
-    # Truncation logic / Truncated MVN instead of MVN
-    i = 0
-    while i < steps:
-        i += 1
-        batch = np.random.multivariate_normal(mean=mu, cov=cov, size=(batch_size, size))
-        batch_tf = tf.convert_to_tensor(batch)  # dims: (batch_size, size, 3)
-        yield (batch_tf, tf.constant([1]))  # (data, label)
+# Helper libraries
+import numpy as np
+import pandas as pd
+from numba import cuda
+import os
 
 
+
+
+
+# Heteroscedasktic loss function
+
+def heteroscedastic_loss(true, pred):
+    """ Heteroskedastic loss function."""
+    params = pred.shape[1] // 2
+    point = pred[:, :params]
+    var = pred[:, params:]
+    precision = 1 / var
+    tf.autograph.experimental.do_not_convert
+    return keras.backend.sum(precision * (true - point) ** 2. + keras.backend.log(var), -1)
+
+# Create Model ( will be changed for taking on the fly generated tensors from M3_generator function
+def create_model(n_params, channels, filters, batchnorm=False, training=True):
+    """Creates 1DConv model."""
+
+    inp = keras.Input(shape=(None, channels))
+    x = inp
+    x = keras.layers.Conv1D(filters[0], kernel_size=1, strides=1, activation='relu')(x)
+
+    for f in filters[1:]:
+        x = keras.layers.Conv1D(f, kernel_size=3, strides=2, activation='relu')(x)
+        # x = keras.layers.Dropout(rate=0.2)(x,training=True)
+        if batchnorm:
+            x = keras.layers.BatchNormalization()(x, training=training)
+
+    x = keras.layers.GlobalAveragePooling1D()(x)
+    mean = keras.layers.Dense(n_params)(x)
+    var = keras.layers.Dense(n_params, activation='softplus')(x)
+    out = keras.layers.Concatenate()([mean, var])
+    model = keras.Model(inp, out)
+    return model
+
+# Create Logs for Tensorboard
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+
+# generate adhoc tensors
 dataset = tf.data.Dataset.from_generator(
-    generator=generate_tmvn,
-    args=[128],  # batch_size
+    generator=generate_M3,
+    args=[batchsize],  # batch_size
     output_shapes=(
-        tf.TensorShape([128, 1000, 3]),
+        tf.TensorShape([batchsize, 1000, 5]),
         tf.TensorShape([1, ])
     ),
     output_types=(tf.float32, tf.int32))
 
-model = models.Sequential([
-    layers.Dense(4),
-    layers.Conv1D(filters=8, kernel_size=3, padding="same"),
-    layers.Flatten(),
-    layers.Dense(1),
-])
-model.compile(loss="mean_squared_error")
-model.build(input_shape=(1, 1000, 3))
-model.summary()
 
-model.fit(dataset, epochs=10)
+
+# Training
+keras.backend.clear_session()
+deepInference = create_model(n_params=len(NAMES), channels=nchannels, filters=filters)
+deepInference.summary()
+
+deepInference.compile(optimizer=optimizer, loss=heteroscedastic_loss, metrics=['accuracy'])
+earlystopper = keras.callbacks.EarlyStopping(monitor='val_loss', patience=2, verbose=2)
+checkpointer = keras.callbacks.ModelCheckpoint(checkpoint_path, save_best_only=True, save_weights_only=False, verbose=2)
+
+# Fit Model
+
+ history = deepInference.fit(M3_data, epochs=10
+                                epochs=10,
+                                validation_split=0.01,
+                                callbacks=[earlystopper, checkpointer,tensorboard_callback])
+
+
+# Make Predictions from Test Data
+preds = deepInference.predict(####)
+
+
+
+
+
+
+
+
+# Scratch Code
+#
+# dataset = tf.data.Dataset.from_generator(
+#     generator=generate_tmvn,
+#     args=[128],  # batch_size
+#     output_shapes=(
+#         tf.TensorShape([128, 1000, 3]),
+#         tf.TensorShape([1, ])
+#     ),
+#     output_types=(tf.float32, tf.int32))
+#
+# model = models.Sequential([
+#     layers.Dense(4),
+#     layers.Conv1D(filters=8, kernel_size=3, padding="same"),
+#     layers.Flatten(),
+#     layers.Dense(1),
+# ])
+# model.compile(loss="mean_squared_error")
+# model.build(input_shape=(1, 1000, 3))
+# model.summary()
+#
+# model.fit(dataset, epochs=10)
